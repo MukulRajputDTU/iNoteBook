a9cdcd33e26b003e50809406b02793448160c3af
Mukul <mukulrajputnap@gmail.com>
2025-08-15 19:50:09 +0530

    commit2

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,17 +1,91 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast, Bounce } from "react-toastify";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function SignUp() {
-  const [credentials, setCredentials] = useState({ name: "", email: "", password: "", cpassword: "" });
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: "",
+  });
   const navigate = useNavigate();
+
+  const notify = (message, type) => {
+    if (type === "warn") {
+      toast.warn(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+    else if (type === "error") {
+      toast.error(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+    else if (type === "success") {
+      toast.success(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+    else {
+      toast.default(message, {
+        position: "top-center",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: false,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+      });
+    }
+  };
+
+  const validateEmail = (email) => {
+    const regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return regex.test(email);
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!validateEmail(credentials.email)) {
+      notify("Invalid Email", "error");
+      return;
+    }
+    if (credentials.password.length < 5) {
+      notify("Password must be 5 characters long.", "error");
+      return;
+    }
     if (credentials.password !== credentials.cpassword) {
-      alert("Passwords do not match");
+      notify("Passwords do not match", "warn");
       return;
     }
     const response = await fetch("http://localhost:5000/api/auth/createuser", {
@@ -22,7 +96,7 @@ export default function SignUp() {
       body: JSON.stringify({
         name: credentials.name,
         email: credentials.email,
-        password: credentials.password
+        password: credentials.password,
       }),
     });
 
@@ -30,19 +104,21 @@ export default function SignUp() {
     console.log(json);
 
     if (json.success) {
-      localStorage.setItem("token", json.authtoken);
-      navigate("/");
-    }
-    else {
-      alert("User already exists!")
+      navigate('/login', { state: { fromSignup: true } });
+    } else {
+      notify("User already exists!","error");
     }
   };
-   
+
   const onChange = (e) => {
     setCredentials({ ...credentials, [e.target.name]: e.target.value });
   };
+  
   return (
-    <div className="container d-flex justify-content-center align-items-center" style={{ minHeight: "80vh", marginTop: "30px" }}>
+    <div
+      className="container d-flex justify-content-center align-items-center"
+      style={{ minHeight: "80vh", marginTop: "30px" }}
+    >
       <motion.div
         className="shadow rounded-4 p-5 bg-white w-100"
         style={{ maxWidth: "400px" }}
@@ -54,28 +130,78 @@ export default function SignUp() {
         <form onSubmit={handleSubmit}>
           <div className="mb-3">
             <label>Name</label>
-            <input type="text" className="form-control" id="name" name="name" onChange={onChange} placeholder="Your Name" required />
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              name="name"
+              onChange={onChange}
+              placeholder="Your Name"
+              required
+            />
           </div>
           <div className="mb-3">
             <label>Email</label>
-            <input type="email" className="form-control" id="email" name="email" onChange={onChange} placeholder="Enter email" required />
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              name="email"
+              onChange={onChange}
+              placeholder="Enter email"
+              required
+            />
           </div>
           <div className="mb-3">
             <label>Password</label>
-            <input type="password" className="form-control" id="password" name="password" onChange={onChange} placeholder="Password" required />
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              name="password"
+              onChange={onChange}
+              placeholder="Password"
+              required
+            />
           </div>
           <div className="mb-3">
             <label>Confirm Password</label>
-            <input type="password" className="form-control" id="cpassword" name="cpassword" onChange={onChange} placeholder="Confirm Password" required />
+            <input
+              type="password"
+              className="form-control"
+              id="cpassword"
+              name="cpassword"
+              onChange={onChange}
+              placeholder="Confirm Password"
+              required
+            />
           </div>
-          <button type="submit" className="btn btn-success w-100">Sign Up</button>
+          <button type="submit" className="btn btn-success w-100">
+            Sign Up
+          </button>
         </form>
         <p className="text-center mt-3">
           Already have an account?{" "}
-          <Link to="/login" style={{ cursor: "pointer", textDecoration: "underline" }}>
+          <Link
+            to="/login"
+            style={{ cursor: "pointer", textDecoration: "underline" }}
+          >
             Login
           </Link>
         </p>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnFocusLoss={false}
+          draggable
+          pauseOnHover={false}
+          theme="colored"
+          transition={Bounce}
+        />
       </motion.div>
     </div>
   );
